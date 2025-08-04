830ea728cb73dfc6363db7b185eb9823f618627c
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-08-04 19:59:04 +0200

    Update stats.php.com.net.condition.URL_Shortener_Data.php

--- tifsec.github.io/zu.ct.ws/stats.php.com.net.condition.URL_Shortener_Data.php
+++ tifsec.github.io/zu.ct.ws/stats.php.com.net.condition.URL_Shortener_Data.php
@@ -1,5 +1,5 @@
 <?php
 if (isset($_GET['shorten_url']) && $_GET['shorten_url'] === "3IBPBlVQuRb") {
-    echo "<p><strong>https://t.co/arTvx4sAiz</strong></p>";
+    echo "https://t.co/arTvx4sAiz";
 }
 ?>
